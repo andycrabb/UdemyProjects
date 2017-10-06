c7910eead45673b4a20d86c71325bdd1ed8643db
andycrabb <andycrabby@hotmail.com>
2017-10-06 07:48:27 +0000

    adding Mongo to YelpCamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,24 +1,34 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-
-var campgrounds = [
-    {name:"Port Vincent", image:"http://haileyidaho.com/wp-content/uploads/2015/01/Stanley-lake-camping-Credit-Carol-Waller-2011.jpg"},
-    {name:"Norlunga", image:"http://www.christiesbeachtouristpark.com.au/images/CBTP_aerial3.jpg"},
-    {name:"Victor Harbour", image:"http://www.britz.com.au/travel-guide-australia/camping-australia/PublishingImages/Pages/default/Britz-camping-australia.jpg?RenditionID=31"},
-    {name:"Largs Bay", image:"https://www.discoveryholidayparks.com.au/file/resize/c470x300~80/1839_adelaide_beachfront_park_caravan_site_1_hdr_lowquality.jpg"}
- ];
-
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+ 
+mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+
+//SCHEMA 
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
    res.render("landing");
 });
 
+//Index - show all campgrounds
 app.get("/campgrounds", function(req, res) {
-   res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+   
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -26,13 +36,24 @@ app.get("/campgrounds/new", function(req, res){
 });
 
 app.post("/campgrounds", function(req, res){
-    var name = req.body.image;
+    var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
+    
 });
 
-app.listen(8081, function(){
+// app.listen(8081, function(){
+//      console.log("Yelp Server is Running!");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
      console.log("Yelp Server is Running!");
 });
