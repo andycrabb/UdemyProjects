b74f9b6403afd6ee26bfce870d54dbd6e208a551
andycrabb <andycrabby@hotmail.com>
2017-10-16 20:52:43 +1030

    adding show.ejs and introduce REST

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -11,6 +11,7 @@ app.set("view engine", "ejs");
 var campgroundSchema = new mongoose.Schema({
    name: String,
    image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -26,7 +27,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-          res.render("campgrounds",{campgrounds:allCampgrounds});
+          res.render("index.ejs",{campgrounds:allCampgrounds});
        }
     });
 });
@@ -36,7 +37,8 @@ app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -66,6 +68,6 @@ app.get("/campgrounds/:id", function(req, res){
     });
 })
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 8081, process.env.IP,  function(){
    console.log("The YelpCamp Server Has Started!");
 });
