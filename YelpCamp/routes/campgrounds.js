d1dba2cc37d3f4bf536a8233e3b234c099677851
andycrabb <andycrabby@hotmail.com>
2018-08-21 21:03:55 +0930

    adding a delete route and button

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -78,6 +78,18 @@ router.put("/:id", function(req, res){
 //redirect somewhere(show page)
 });
 
+//Destroy campground route
+router.delete("/:id", function(res, req){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+          res.redirect("/campgrounds");
+       } else {
+            res.redirect("/campgrounds");
+       }
+    });
+});
+
+
 //UPDATE CAMPGROUND ROUTE!
 function isLogedIn(req, res, next){
     if(req.isAuthenticated()){
